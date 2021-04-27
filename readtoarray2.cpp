610427a986219da4b7d7659ddbafa4b5b9e0fa25
icebowl <icebowl1967@gmail.com>
2021-04-26 20:39:31 -0700

    AA

---
+++ readtoarray2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <array>
+
+int main()
+{
+    using namespace std;
+	int i;
+	const int  array_length = 9897;
+	array<string,array_length> wordlist;
+	//string wordlist[26];
+    ifstream filename("10000.txt");
+    if(filename.is_open())
+    {   for(i = 0; i < array_length; ++i)
+        {
+            filename >> wordlist[i];
+        }
+    }
+    // print list of words
+    for(i = 0; i < array_length;i++){
+			cout<<wordlist[i]<<endl;
+	}
+
+}
