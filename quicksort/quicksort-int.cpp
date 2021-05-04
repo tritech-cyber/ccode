8a6fa21da608b66d0c602b09cfd467de7d09cba1
icebowl <icebowl1967@gmail.com>
2021-05-03 21:10:25 -0700

    AA

--- quicksort/quicksort-int.cpp
+++ quicksort/quicksort-int.cpp
@@ -1,9 +1,8 @@
 //https://www.geeksforgeeks.org/quick-sort/
 #include <iostream>
-#include <array>
-#include <string>
+
 using namespace std;
-const int arraylength = 10;
+ 
 // A utility function to swap two elements
 void swap(int* a, int* b)
 {
@@ -17,9 +16,9 @@ the pivot element at its correct position in sorted
 array, and places all smaller (smaller than pivot)
 to left of pivot and all greater elements to right
 of pivot */
-int partition (array<string,arraylength>list, int low, int high)
+int partition (int arr[], int low, int high)
 {
-    int pivot = list[high]; // pivot
+    int pivot = arr[high]; // pivot
     int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
  
     for (int j = low; j <= high - 1; j++)
@@ -28,10 +27,10 @@ int partition (array<string,arraylength>list, int low, int high)
         if (arr[j] < pivot)
         {
             i++; // increment index of smaller element
-            swap(&list[i], &list[j]);
+            swap(&arr[i], &arr[j]);
         }
     }
-    swap(&list[i + 1], &list[high]);
+    swap(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
  
@@ -39,23 +38,22 @@ int partition (array<string,arraylength>list, int low, int high)
 arr[] --> Array to be sorted,
 low --> Starting index,
 high --> Ending index */
-array<string,arraylength>  quickSort(array<string,arraylength>list, int low, int high)
+void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
         /* pi is partitioning index, arr[p] is now
         at right place */
-        int pi = partition(list, low, high);
+        int pi = partition(arr, low, high);
  
         // Separately sort elements before
         // partition and after partition
-        quickSort(list, low, pi - 1);
-        quickSort(list, pi + 1, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
-    return list;
 }
  
-/* Function to print an array 
+/* Function to print an array */
 void printArray(int arr[], int size)
 {
     int i;
@@ -63,16 +61,15 @@ void printArray(int arr[], int size)
         cout << arr[i] << " ";
     cout << endl;
 }
- */
  
+// Driver Code
 int main()
 {
-    array<string, arraylength> thelist {"abcdef","bcdefg","cdefgh","defghi","efghij",
-										"ghijkl","hijklm","ijklmn","jklmno","klmnop"};
-	int n = thelist.size();
-    quickSort(thelist, 0, n - 1);
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
     cout << "Sorted array: \n";
-   // printArray(thelist, n);
+    printArray(arr, n);
     return 0;
 }
  
