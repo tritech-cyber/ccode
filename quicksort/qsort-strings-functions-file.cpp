c1ed98aa2f022970248324cfcddeaf12a22e06a8
cwc <icebowl1967@gmail.com>
2021-05-04 19:01:46 -0700

    AA

---
+++ quicksort/qsort-strings-functions-file.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace::std;
+
+//  function will display on cout the contents of an arrary -  FUNCTION 1
+void printArray( ostream & out, const string data[], int cellsUsed);
+
+
+//  function will sort an array                             -  FUNCTION 2
+void sortArray( string data[], int cellsUsed);
+
+
+
+int main()
+{
+	const int CELLS = 5;
+	string  names[CELLS] = { "tom", "mary", "ann", "bill","carol"};
+     ifstream filename("20000.txt");
+    if(filename.is_open())
+    {   for(i = 0; i < arraylength; ++i)
+        {
+            filename >> wordlist[i];
+        }
+    }
+	cout << "Original array" << endl;
+	printArray(cout,names,CELLS);
+	cout << endl;
+
+	sortArray(names,CELLS);
+
+	cout << "Sorted array" << endl;
+	printArray(cout,names,CELLS);
+	cout << endl;
+
+	if (names[0][0] >  names[1][0])
+		cout << "YAY" << endl;
+	return 0;
+}   // end main
+
+
+//  write FUNCTION 1
+void printArray( ostream & out, const string data[], int cellsUsed)
+{
+	for (int i=0; i <cellsUsed;i++)
+	{
+		out << data[i] << endl;
+	}
+}
+
+
+//  write FUNCTION 2
+void sortArray( string data[], int cellsUsed)
+{
+	sort(data, data+ cellsUsed);
+
+}
