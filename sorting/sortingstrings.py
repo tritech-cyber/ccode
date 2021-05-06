0fbba3f1b23ccc0711f4dea60ee35f8fab794f7d
tritechsc <craig.coleman@ksd.org>
2021-05-06 10:25:10 -0700

    AA

--- sorting/sortingstrings.py
+++ sorting/sortingstrings.py
@@ -3,7 +3,7 @@ import time
 
 timestamp1 = int(time.time()*1000.0)
 print(timestamp1)
-time.sleep(1)
+#time.sleep(1)
 timestamp2 = int(time.time()*1000.0)
 print(timestamp2)
 print(str(timestamp2-timestamp1)+str(" ms "))
@@ -18,28 +18,26 @@ csort = "./csort.o"
 print("Insertion Sort")
 timestamp1 = int(time.time()*1000.0)
 os.system(isort)
-time.sleep(1)
+#time.sleep(1)
 timestamp2 = int(time.time()*1000.0)
 isorttime = (timestamp2-timestamp1)/1000
 print(str(timestamp2-timestamp1)+str(" ms "))
-time.sleep(4)
+#time.sleep(4)
 
 # selection sort
 print("Selection Sort")
 timestamp1 = int(time.time()*1000.0)
-os.system(ssort)
-time.sleep(1)
+#time.sleep(1)
 os.system(ssort)
 timestamp2 = int(time.time()*1000.0)
 ssorttime = (timestamp2-timestamp1)/1000
 print(str(timestamp2-timestamp1)+str(" ms "))
-time.sleep(4)
+#time.sleep(4)
 
 # c sort
 print("C Sort (Quick Sort")
 timestamp1 = int(time.time()*1000.0)
-os.system(ssort)
-time.sleep(1)
+#time.sleep(1)
 os.system(csort)
 timestamp2 = int(time.time()*1000.0)
 csorttime = (timestamp2-timestamp1)/1000
@@ -49,3 +47,4 @@ print(" * * * * * * * * ")
 print("Insertion sort " + str(isorttime))
 print("Selection sort " + str(ssorttime))
 print("C sort         " + str(csorttime))
+
