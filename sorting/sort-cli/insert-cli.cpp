02bd0f872f82bc719da7b79deb9d6ba8063e195e
tritechsc <craig.coleman@ksd.org>
2021-05-07 13:14:46 -0700

    AA

--- sorting/sort-cli/insert-cli.cpp
+++ sorting/sort-cli/insert-cli.cpp
@@ -1,51 +1,29 @@
 #include <iostream>
-#include <fstream>
-#include <array>
 #include <string>
-
+#include <fstream>
 
 using namespace std;
-//const int arraylength = 26;
-const int  arraylength = 10000;
 
-array<string,arraylength> insertSort(array<string,arraylength>list);
-void printArray(array<string,arraylength>list);
-
-
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    cout<< argv[1]<<endl;
-    int thelength = stoi(argv[1]);	
-	int i;
-	string p;
-	array<string,arraylength> wordlist;
-    ifstream filename("10000.txt");
+	int i,j;
+	const int arraylength = stoi(argv[1]);	
+	string list[arraylength];
+	string wordlistlen = argv[1];
+	ifstream filename(wordlistlen + ".txt");
     if(filename.is_open())
     {   for(i = 0; i < arraylength; ++i)
         {
-            filename >> wordlist[i];
+            filename >> list[i];
         }
     }
-							
-	// sorting
-	array<string,arraylength>arraysorted;
-	//cout<<"Array unsorted"<<endl;
-	//printArray(wordlist);
-	//cout<<"\n\nInsertion Sort"<<endl;
-	arraysorted = insertSort(wordlist);
-	//printArray(arraysorted);
-	cout<<"Insert Sort Completed"<<endl;
-    return 0;
-}
-
-
-array<string,arraylength> insertSort(array<string,arraylength>list){
-	int i,j;
+	// end of loading file to array						
+	// insert sort
 	int p;
 	int noswap = 0;
 	int compare = 0;
 	string temp;
-	int n = list.size();//array length
+	int n = arraylength;
                 
 	for (i = 0; i < n; i++){
 		j = i;
@@ -57,17 +35,13 @@ array<string,arraylength> insertSort(array<string,arraylength>list){
 				j--;
 				noswap++;
 			}
-			//cout << " loop i "<<i<<endl;
-			//cin>>p;
-			//printArray(list);
-	}
+	}//end for i in insert sort
 
-	return list;
-}
-
-void printArray(array<string,arraylength>list){
-	for (int i = 0;i < arraylength ;i++){
+	for (i = 0;i < arraylength ;i++){
 		cout<<list[i]<<" "<<endl;
 		//if(i % 13 == 0 && i != 0)cout<<endl;
 	}
+	
+	cout<<"\n\n Insertion sort - arrayLength "<<arraylength<<endl;
+    return 0;
 }
